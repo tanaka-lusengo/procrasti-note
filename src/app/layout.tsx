43acd02751c47feabbc88a132de6692a9f3f9c68
tanakalusengo <tanaka.lusengo@krakentechnologies.ltd>
2023-12-19 17:20:57 +0000

    style: add nav component and shard ui component:
    - add shared StyledContainer for consistent responsive design

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { ThemeProvider } from "styled-components";
 import { StyledComponentsRegistry } from "@/lib";
 import { GlobalStyles, theme } from "@/styles";
+import { Nav, StyledContainer } from "@/components";
 
 export const metadata: Metadata = {
   title: "Procrasti-Not(e)",
@@ -14,7 +15,12 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => (
     <body>
       <StyledComponentsRegistry>
         <GlobalStyles />
-        <ThemeProvider theme={theme}>{children}</ThemeProvider>
+        <ThemeProvider theme={theme}>
+          <StyledContainer>
+            <Nav />
+            {children}
+          </StyledContainer>
+        </ThemeProvider>
       </StyledComponentsRegistry>
     </body>
   </html>
---
+++ src/components/index.ts
@@ -0,0 +1,2 @@
+export { default as Nav } from "./Nav";
+export { default as StyledContainer } from "./ui/StyledContainer/index.styled";
