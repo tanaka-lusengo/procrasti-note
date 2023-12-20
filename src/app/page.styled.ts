8272177448c8f1a9ffda7ea7b482116cda3f0916
tanakalusengo <tanaka.lusengo@krakentechnologies.ltd>
2023-12-20 16:02:32 +0000

    refactor(style): update homepage styling and contect

--- src/app/page.styled.ts
+++ src/app/page.styled.ts
@@ -1,6 +1,29 @@
-"use client";
 import styled from "styled-components";
 
-export const Title = styled.h1`
-  text-align: center;
+export const Section = styled.section`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  gap: 4rem;
+
+  margin: 4rem 0;
+
+  a {
+    color: #ff6161;
+    font-weight: bold;
+    font-size: ${({ theme }) => theme.fontSize.h4}rem;
+    border-bottom: 2px solid transparent;
+    transition: border-bottom 0.3s ease-in-out;
+
+    &:hover {
+      border-bottom: 2px solid #ff6161;
+    }
+  }
+
+  @media only screen and (min-width: ${({ theme }) => theme.breakpoints.lg}px) {
+    p {
+      max-width: 50%;
+    }
+  }
 `;
