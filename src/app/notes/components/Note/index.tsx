a880a58402a8070fb5017fe7ef7123628922a582
tanakalusengo <tanaka.lusengo@krakentechnologies.ltd>
2023-12-21 01:32:23 +0000

    feat: add components to list data from database: - Notes homepage - NotesContainer component - Notes component

---
+++ src/app/notes/components/Note/index.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSeedling } from "@fortawesome/free-solid-svg-icons";
+import { useTheme } from "styled-components";
+import { Typography } from "@/components";
+import * as Styled from "./index.styled";
+
+type Category = "personal" | "productivity";
+
+interface NoteProps {
+  title: string;
+  category: Category;
+}
+
+const Note = ({ title, category }: NoteProps) => {
+  const { colors } = useTheme();
+
+  return (
+    <Styled.ListCard>
+      <FontAwesomeIcon icon={faSeedling} style={{ color: colors.primary }} />
+      <div>
+        <Typography tag="h5">{title}</Typography>
+        <Typography tag="h6">- {category}</Typography>
+      </div>
+    </Styled.ListCard>
+  );
+};
+
+export default Note;
