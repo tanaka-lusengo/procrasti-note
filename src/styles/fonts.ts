0701dfdbc1ac13e76ce7785d616ed5738801e0a4
tanakalusengo <tanaka.lusengo@krakentechnologies.ltd>
2023-12-24 23:46:34 +0000

    fix(test): add mocks for google fonts from nextjs

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,6 +1,6 @@
-import { Slabo_27px as Slabo, Ultra } from 'next/font/google';
+import { Slabo_27px, Ultra } from 'next/font/google';
 
-export const slabo = Slabo({
+export const slabo = Slabo_27px({
   weight: '400',
   subsets: ['latin'],
   fallback: ['serif'],
--- src/tests/vitest.setup.ts
+++ src/tests/vitest.setup.ts
@@ -1,2 +1,19 @@
+import { Slabo_27px, Ultra } from 'next/font/google';
+import { vi } from 'vitest';
+
 import '@testing-library/jest-dom/vitest';
 import 'vitest-canvas-mock';
+
+// Mock the next/font/google module
+vi.mock('next/font/google', () => ({
+  Ultra: () => ({
+    style: {
+      fontFamily: Ultra,
+    },
+  }),
+  Slabo_27px: () => ({
+    style: {
+      fontFamily: Slabo_27px,
+    },
+  }),
+}));
