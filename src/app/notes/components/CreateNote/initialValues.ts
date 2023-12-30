07677d794326b65bfdd390013d4ade9055cfeb2e
tanakalusengo <tanaka.lusengo@krakentechnologies.ltd>
2023-12-30 01:23:04 +0000

    refactor: add new flexible NoteForm component

--- src/app/notes/components/CreateNote/initialValues.ts
+++
@@ -1,5 +0,0 @@
-export const initialValues = {
-  title: '',
-  category: '',
-  content: '',
-};
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 // Components
 export { default as Nav } from './Nav';
+export { default as NoteForm } from './NoteForm';
 
 // General UI Components
 export { Button, ButtonLink, Container, Typography } from './ui';
