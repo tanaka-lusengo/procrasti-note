e849b7d7aa4cc0afbaefadc0e3274f5e4435847e
tanakalusengo <tanaka.lusengo@krakentechnologies.ltd>
2024-01-17 22:59:30 +0000

    database: update collection rules and fields

---
+++ pb_migrations/1704212004_updated_users.js
@@ -0,0 +1,33 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("_pb_users_auth_")
+
+  // remove
+  collection.schema.removeField("u6cmk0nj")
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("_pb_users_auth_")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "u6cmk0nj",
+    "name": "notes",
+    "type": "relation",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "collectionId": "5q65b3e3fwuhj76",
+      "cascadeDelete": false,
+      "minSelect": null,
+      "maxSelect": null,
+      "displayFields": null
+    }
+  }))
+
+  return dao.saveCollection(collection)
+})
---
+++ pb_migrations/1704212808_updated_notes.js
@@ -0,0 +1,22 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("5q65b3e3fwuhj76")
+
+  collection.listRule = ""
+  collection.viewRule = ""
+  collection.updateRule = ""
+  collection.deleteRule = ""
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("5q65b3e3fwuhj76")
+
+  collection.listRule = "@request.auth.id != \"\" && author = @request.auth.id"
+  collection.viewRule = "@request.auth.id != \"\" && author = @request.auth.id"
+  collection.updateRule = "@request.auth.id != \"\" && author = @request.auth.id"
+  collection.deleteRule = "@request.auth.id != \"\" && author = @request.auth.id"
+
+  return dao.saveCollection(collection)
+})
