d51b67f2675ea6d310c7ef1a2350ac49b647875a
tanakalusengo <tanaka.lusengo@krakentechnologies.ltd>
2024-03-05 23:07:40 +0000

    chore(client): update toast config

--- client/src/utils/toastConfig/index.ts
+++ client/src/utils/toastConfig/index.ts
@@ -1,3 +1,4 @@
+import toast from 'react-hot-toast';
 import { CSSProperties } from 'styled-components/dist/types';
 
 interface ToastConfig {
@@ -5,7 +6,11 @@ interface ToastConfig {
   style: { textAlign: CSSProperties['textAlign'] };
 }
 
-export const toastConfig: ToastConfig = {
+const toastConfig: ToastConfig = {
   duration: 3000,
   style: { textAlign: 'center' },
 };
+
+// Toast Notifications
+export const toastNotifyError = (message: string) =>
+  toast.error(`There was an error ${message}`, toastConfig);
