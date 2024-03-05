3f2d7225ecc27a8692248c7f160f28475114da69
tanakalusengo <tanaka.lusengo@krakentechnologies.ltd>
2024-03-05 23:30:14 +0000

    refactor(client): main crud components: - CreateNote - EditNote - DeleteNote

--- client/src/app/notes/components/EditNote/index.tsx
+++ client/src/app/notes/components/EditNote/index.tsx
@@ -1,63 +1,53 @@
 'use client';
 
+import { Toaster } from 'react-hot-toast';
 import { useRouter } from 'next/navigation';
-import { RecordModel } from 'pocketbase';
 
 import { Button, NoteForm } from '@/components';
-import { logErrorMessage } from '@/utils';
+import { type Notes, type NotesUpdate } from '@/lib/openapi/generated';
+import { handleEditNote } from '@/services/notesServices';
 
 import { CommonButtonsContainer } from '../common.styled';
 import CreateAndEditFormFields from '../CreateAndEditFormFields';
-import { FormValues } from '../CreateAndEditFormFields/types';
 import { validationSchema } from '../CreateAndEditFormFields/validationSchema';
 
 interface EditNoteProps {
-  note: Partial<RecordModel>;
+  note: Notes;
   showForm: boolean;
 }
 
-const handleEditNote = async (id: string, values: FormValues) => {
-  try {
-    const { title, category, content } = values || {};
-
-    const editNoteData = {
-      title,
-      category,
-      content,
-    };
-
-    // TODO: This is a temporary console.log
-    // eslint-disable-next-line no-console
-    console.log('Handle edit note', id, editNoteData);
-  } catch (error) {
-    logErrorMessage(error, 'editing note 😿');
-  }
-};
-
 const EditNote = ({ note, showForm }: EditNoteProps) => {
-  const { id, title, category, content } = note || {};
-  const initialValues: FormValues = {
-    title,
-    category,
-    content,
-  };
+  const { id, title, priority, content, complete } = note || {};
+
   const router = useRouter();
 
+  const initialValues: NotesUpdate = {
+    title,
+    priority,
+    content,
+    complete,
+  };
+
   return (
-    <NoteForm
-      initialValues={initialValues}
-      validationSchema={validationSchema}
-      onSubmit={(values) => handleEditNote(id as string, values)}
-      showForm={showForm}
-    >
-      <CreateAndEditFormFields />
-      <CommonButtonsContainer>
-        <Button type="submit">Edit Note</Button>
-        <Button onClick={router.back} type="button">
-          Close
-        </Button>
-      </CommonButtonsContainer>
-    </NoteForm>
+    <>
+      <NoteForm
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={(values) => handleEditNote(id, values, router)}
+        showForm={showForm}
+      >
+        <CreateAndEditFormFields />
+
+        <CommonButtonsContainer>
+          <Button type="submit">Edit Note</Button>
+          <Button onClick={router.back} type="button">
+            Close
+          </Button>
+        </CommonButtonsContainer>
+      </NoteForm>
+
+      <Toaster />
+    </>
   );
 };
 
