6b6a105682a53328b19bd6898a87abe7e4433ac6
tanakalusengo <tanaka.lusengo@krakentechnologies.ltd>
2024-03-08 22:31:38 +0000

    chore(server): update folder structure and error handling

--- server/routers/note.py
+++ server/routers/note.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, HTTPException, Path
 from starlette import status
 from database import db_dependency
-from schemas.schemas import Note, NoteCreate, NoteUpdate
+from schemas.note import Note, NoteCreate, NoteUpdate
 from models.models import Note as NoteModel
 
 router = APIRouter(
@@ -11,7 +11,11 @@ router = APIRouter(
 
 @router.get('/notes', status_code=status.HTTP_200_OK, response_model=list[Note])
 async def get_all_notes(db: db_dependency):
-    return db.query(NoteModel).all()
+    try:
+        return db.query(NoteModel).all()
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail=f"An error occurred while trying to retrieve all notes from the database: {str(e)}")
 
 
 @router.get('/note/{note_id}', status_code=status.HTTP_200_OK, response_model=Note)
@@ -19,18 +23,23 @@ async def get_one_note(db: db_dependency, note_id: int = Path(gt=0)):
     single_note = db.query(NoteModel).filter(NoteModel.id == note_id).first()
 
     if single_note is None:
-        raise HTTPException(status_code=404, detail="Note not found")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"Note with id: {note_id} was not found in the database.")
     else:
         return single_note
 
 
 @router.post('/note', status_code=status.HTTP_201_CREATED)
 async def create_note(db: db_dependency, note_create: NoteCreate):
-    new_note_model = NoteModel(**note_create.model_dump())
-    db.add(new_note_model)
-    db.commit()
-    db.refresh(new_note_model)
-    return new_note_model
+    try:
+        new_note_model = NoteModel(**note_create.model_dump())
+        db.add(new_note_model)
+        db.commit()
+        db.refresh(new_note_model)
+        return new_note_model
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail=f"An error occurred while trying to create a new note: {str(e)}")
 
 
 @router.put('/note/{note_id}', status_code=status.HTTP_204_NO_CONTENT)
@@ -39,7 +48,8 @@ async def update_note(db: db_dependency, note_update: NoteUpdate, note_id: int =
         NoteModel.id == note_id).first()
 
     if new_note_model is None:
-        raise HTTPException(status_code=404, detail="Note not found")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"Note with id: {note_id} could not be found for update.")
     else:
         new_note_model.title = note_update.title
         new_note_model.content = note_update.content
@@ -56,7 +66,8 @@ async def delete_one_note(db: db_dependency, note_id: int = Path(gt=0)):
     single_note = db.query(NoteModel).filter(NoteModel.id == note_id).first()
 
     if single_note is None:
-        raise HTTPException(status_code=404, detail="Note not found")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"Note with id: {note_id} could not be found for deletion.")
     else:
         db.delete(single_note)
         db.commit()
