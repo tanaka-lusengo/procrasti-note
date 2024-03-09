1991646755103b2cccb02a5a1ef0e6c3a90cfd30
tanakalusengo <tanaka.lusengo@krakentechnologies.ltd>
2024-03-09 23:07:11 +0000

    feat(server): add user model, schemas and reflect changes

--- server/models/models.py
+++ server/models/models.py
@@ -1,5 +1,18 @@
 from database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    email = Column(String, unique=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    admin = Column(Boolean, default=False)
 
 
 class Note(Base):
@@ -10,3 +23,5 @@ class Note(Base):
     content = Column(String)
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
+    author_id = Column(Integer, ForeignKey(
+        'user.id', ondelete='CASCADE', onupdate='CASCADE'))
