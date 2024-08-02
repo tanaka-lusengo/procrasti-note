10c6aa2c8c4b0b9be03b9702e9022dcba8f9b87e
tanakalusengo <tanaka.lusengo@krakentechnologies.ltd>
2024-08-02 17:32:38 +0100

    feat: add react-spinners library for loading animations
    The react-spinners library has been added to the project dependencies. This library provides a collection of loading animations that can be used to enhance the user experience. By incorporating these animations, we can provide visual feedback to users during loading processes, improving perceived performance and overall user satisfaction.

---
+++ src/components/SuspenseLoader/index.styled.ts
@@ -0,0 +1,10 @@
+'use client';
+
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: calc(75vh);
+`;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 // Components
 export { default as Nav } from './Nav';
+export { default as SuspenseLoader } from './SuspenseLoader';
