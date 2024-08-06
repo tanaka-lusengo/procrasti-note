03ef18f390df96a089d7cf1d526c5d9312a2eaf4
tanakalusengo <tanaka.lusengo@krakentechnologies.ltd>
2024-08-06 01:52:39 +0100

    refactor: update ToggleComplete component to handle optimistic updates
    Update the ToggleComplete component to handle optimistic updates when toggling the complete state of a note. This change improves the user experience by providing a smoother and more responsive interface when marking notes as complete or incomplete.

--- src/components/Actions/ToggleComplete/index.tsx
+++ src/components/Actions/ToggleComplete/index.tsx
@@ -1,26 +1,58 @@
 'use client';
 
-import { useState } from 'react';
+import { useOptimistic, useState } from 'react';
+import { revalidatePath } from 'next/cache';
 import Image from 'next/image';
 
 import completeChecked from '@/../public/images/circle-check-solid.svg';
 import completeUnchecked from '@/../public/images/circle-regular.svg';
 import { toggleComplete } from '@/server/actions/note-actions';
 import { type NoteModel } from '@/types';
-import { handleError, StatusCode } from '@/utils';
+import { handleError, StatusCode, toastNotifyError } from '@/utils';
 
 import * as Styled from './index.styled';
 
-const ToggleComplete = ({ note }: { note: NoteModel }) => {
-  const { id, complete } = note || {};
+interface ToggleCompleteProps {
+  note: NoteModel;
+  isInNotes?: boolean;
+}
 
+const ToggleComplete = ({ note, isInNotes }: ToggleCompleteProps) => {
+  const { id, complete } = note || {};
   const [isComplete, setIsComplete] = useState(complete);
 
+  // useOptimistic hook to toggle the complete state of the note
+  const [optimisticIsComplete, addOptimisticIsComplete] = useOptimistic(
+    isComplete,
+    (currentState: boolean) => !currentState,
+  );
+
   const handleAction = async () => {
+    // Toggle the optimistic state immediately
+    addOptimisticIsComplete((prev: boolean) => prev);
+
     try {
-      const { status } = await toggleComplete(id, complete);
-      if (status === StatusCode.SUCCESS) setIsComplete(!isComplete);
+      const { status, error } = await toggleComplete(id, optimisticIsComplete);
+
+      if (status === StatusCode.UNAUTHORIZED) {
+        toastNotifyError(`${error}`);
+        // Revert optimistic update on unauthorized
+        addOptimisticIsComplete((prev: boolean) => prev);
+      } else {
+        // Set the state based on optimistic update success
+        setIsComplete(!optimisticIsComplete);
+      }
     } catch (error) {
+      // Revert optimistic update on failure
+      addOptimisticIsComplete((prev: boolean) => prev);
+
+      // Revalidate path depending on the context
+      if (isInNotes) {
+        revalidatePath('/notes');
+      } else {
+        revalidatePath(`/note/${id}`);
+      }
+
       handleError('ticking off the note 😿', error);
     }
   };
@@ -30,15 +62,15 @@ const ToggleComplete = ({ note }: { note: NoteModel }) => {
       <Styled.CompleteButton
         type="submit"
         aria-label="Toggle complete"
-        $isComplete={isComplete}
+        $isComplete={optimisticIsComplete}
       >
         <Image
-          src={isComplete ? completeChecked : completeUnchecked}
+          src={optimisticIsComplete ? completeChecked : completeUnchecked}
           width={33}
           style={{ height: 'auto' }}
           priority
           alt={
-            isComplete
+            optimisticIsComplete
               ? 'A circle icon with a checkmark'
               : 'A circle icon without a checkmark'
           }
